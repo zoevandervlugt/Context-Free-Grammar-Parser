cd8065ebf5ff5add8b35add1d34ca8969621bce3
ReehaR <119525469+ReehaR@users.noreply.github.com>
2025-03-13 17:46:06 +0000

    More progress

--- main.cpp
+++ main.cpp
@@ -63,38 +63,77 @@ vector<string> readRules(vector<string> content) {
     return rules;
 }
 
+bool findRule(vector<vector<string>> rules, string var, string term) {
+    for(vector<string> rule: rules){
+        if(rule[0] == var) {
+            if(rule.back() == term){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool checkLine(string line, vector<string> variables, vector<string> terminals, vector<vector<string>> rules, string startVar){
+    string table[line.length() - 1][line.length() - 1];
+    if(line == "" || line == " "){
+        return findRule(rules, startVar, "e");
+    }
+    for(int i = 0; i < line.length(); i++){
+        string b = line[i] + "";
+        for(string A: variables){
+            if(findRule(rules, A, b)){
+                table[i][i] = A;
+            }
+        }
+    }
+    for(int l = 1; l < line.length(); l++){
+        for(int i = 0; i < line.length() - l + 1; i++){
+            int j = i + l - 1;
+            for(int k = i; i <= j - 1; k++) {
+                // For each rule that leads to 2 concatenated variables
+                
+            }
+        }
+    }
+}
+
 int main(){
     string grammar;
     string input;
     cin >> grammar >> input;
 
-    // testing file names
-    // cout << grammar << " " << input;
-
-    //change name to grammar contents.
-    vector<string> contents = openFile(grammar);
-    // testing openFile
-    // for(string line: contents){
-    //     cout << line << endl;
-    // }
-
-    vector<string> variables = splitString(contents[0]);
-    // testing splitString
-    // for(string s: variables){
-    //     cout << s << endl;
-    // }
+    vector<string> grammarContents = openFile(grammar);
     
-    
-    vector<string> rules = readRules(contents);
+    vector<string> rules = readRules(grammarContents);
 
-    cout << "Variables: " << contents[0] << endl << endl;
-    cout << "Terminals: " << contents[1] << endl << endl;
+    cout << "Variables: " << grammarContents[0] << endl << endl;
+    cout << "Terminals: " << grammarContents[1] << endl << endl;
     cout << "Rules: " << endl << "------" << endl;
     for(string rule: rules) {
         cout << rule << endl;
     }
     cout << endl;
-    cout << "Start Variable: " << contents.back() << endl;
+    cout << "Start Variable: " << grammarContents.back() << endl;
+
+    vector<string> inputContents = openFile(input);
+    vector<string> variables = splitString(grammarContents[0]);
+    vector<string> terminals = splitString(grammarContents[1]);
+    string startVar = grammarContents.back();
+
+    vector<vector<string>> formattedRules;
+    for(string rule: rules){
+        formattedRules.push_back(splitString(rule));
+    }
+
+    for(string line: inputContents){
+        if(checkLine(line, variables, terminals, formattedRules, startVar)){
+            cout << line << ": Accept" << endl;
+        } else {
+            cout << line << ": Reject" << endl;
+        }
+    }
+
 }
 // g++ main.cpp -o main
 // ./main
